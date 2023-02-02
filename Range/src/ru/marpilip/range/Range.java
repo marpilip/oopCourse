d8ec7ea7b880eb6e148c2a6ed74ef9298398a66e
marpilip <mar.pilip@gmail.com>
2023-02-02 15:55:25 +0700

    Add Range and Shapes projects

--- Range/src/ru/marpilip/range/Range.java
+++ Range/src/ru/marpilip/range/Range.java
@@ -1,4 +1,27 @@
 package ru.marpilip.range;
 
 public class Range {
+    double from;
+    double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
 }
