1d958782d3b42763e407b1acf1376df0d40f16a9
marpilip <mar.pilip@gmail.com>
2023-02-07 17:23:33 +0700

    Add Range and Shapes projects

--- Range/src/ru/marpilip/range_main/Main.java
+++ Range/src/ru/marpilip/range_main/Main.java
@@ -8,19 +8,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Range range = new Range(0, 0);
-
         System.out.println("Введите число, с которого начинается диапазон:");
-        range.setFrom(scanner.nextDouble());
+        double from = scanner.nextDouble();
 
         System.out.println("Введите число, которым заканчивается диапазон:");
-        range.setTo(scanner.nextDouble());
+        double to = scanner.nextDouble();
+
+        Range range = new Range(from, to);
 
         System.out.println("Длина вашего диапазона = " + range.getLength());
 
         System.out.println("Введите число, которое хотите проверить на принадлежность диапазону:");
         double numberInRange = scanner.nextDouble();
-        System.out.println("Число " + numberInRange + " принадлежит заданному диапазону? " + range.isInside(numberInRange));
 
+        System.out.println("Число " + numberInRange + " принадлежит заданному диапазону? " + range.isInside(numberInRange));
     }
 }
